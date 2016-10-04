b216b7e26d207901c8baefcb1b407ecc5eb0d035
RufusLiu <RufusLiu@liuzhaohongde-MacBook-Pro.local>
2016-10-04 17:07:12 +0800

    [modify] 10/04 app.js  login.html

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ app.get('/logined', function (req, res) {
     sess = req.session;
     if (!req.query.username || !req.query.uesrpassword) {
         res.send('login failed');
-    }else if(req.query.username === "amy" || req.query.uesrpassword === "amyspassword"){
+    }else if(req.query.username === "amy" && req.query.uesrpassword === "amyspassword"){
         sess.user = req.query.username ;
         sess.admin = true;
         res.sendfile('./views/logined.html');
 
         console.log("login success!");
+    }else{
+        res.send('login username or password is wrong');
     }
 
     console.log(sess.user);
@@ -29,9 +31,11 @@ app.get('/logined', function (req, res) {
 });
 
 app.get('/logout', function (req, res ) {
-    req.session.destroy();
+    sess = req.session;
+    console.log("1"+sess.user);
+    sess.destroy();
     res.end("logout!");
-
+    console.log("2"+sess.user);
 });
 app.listen(3000);
 console.log("app running at http://localhost:3000");
